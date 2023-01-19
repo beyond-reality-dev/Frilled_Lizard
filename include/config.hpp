eb4b81fe173dbeb4c30f9b154dc5d832f2303f22
cybershipsdevelopment <2024.scott.pawley@gmail.com>
2023-01-19 10:08:48 -0500

    Make formatting consistent

--- include/config.hpp
+++ include/config.hpp
@@ -1,15 +1,15 @@
 #pragma once
 #include "main.h"
 
-// Define the controllers.
+// Declare the controllers.
 extern pros::Controller master;
 
-// Define the sensors.
+// Declare the sensors.
 extern pros::Vision color_sensor;
 extern pros::vision_signature_s_t RED_SIG;
 extern pros::vision_signature_s_t BLUE_SIG;
 
-// Define the drivetrain motors.
+// Declare the drivetrain motors.
 extern pros::Motor left_back;
 extern pros::Motor left_front;
 extern pros::Motor right_back;
@@ -17,10 +17,10 @@ extern pros::Motor right_front;
 extern pros::Motor_Group left_wheels;
 extern pros::Motor_Group right_wheels;
 
-// Define the other motors.
+// Declare the other motors.
 extern pros::Motor intake;
 extern pros::Motor roller;
 extern pros::Motor launcher;
 
-// Define variables.
+// Declare variables.
 extern std::string target_color;
