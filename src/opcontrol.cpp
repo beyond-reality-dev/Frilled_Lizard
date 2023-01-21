1b4bb9a83413745d0134f041a2dd61ba3523b591
cybershipsdevelopment <2024.scott.pawley@gmail.com>
2023-01-21 07:17:47 -0500

    Add config file

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -16,31 +16,6 @@
  * task, not resume it from where it left off.
  */
 
-// Define ports for motors and devices.
-#define LEFT_BACK_PORT 3
-#define LEFT_FRONT_PORT 4
-#define RIGHT_BACK_PORT -1
-#define RIGHT_FRONT_PORT -2
-#define INTAKE_PORT 5
-#define ROLLER_PORT 10
-#define LAUNCHER_PORT 7
-
-// Configure controllers.
-pros::Controller master(pros::E_CONTROLLER_MASTER);
-
-// Configure drivetrain motors.
-pros::Motor left_back(LEFT_BACK_PORT, pros::E_MOTOR_GEARSET_18, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor left_front(LEFT_FRONT_PORT, pros::E_MOTOR_GEARSET_18, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor right_back(RIGHT_BACK_PORT, pros::E_MOTOR_GEARSET_18, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor right_front(RIGHT_FRONT_PORT, pros::E_MOTOR_GEARSET_18, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor_Group left_wheels({left_back, left_front});
-pros::Motor_Group right_wheels({right_back, right_front});
-
-// Configure other motors.
-pros::Motor intake(INTAKE_PORT, pros::E_MOTOR_GEARSET_18, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor roller(ROLLER_PORT, pros::E_MOTOR_GEARSET_36, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor launcher(LAUNCHER_PORT, pros::E_MOTOR_GEARSET_36, pros::E_MOTOR_ENCODER_DEGREES);
-
 void opcontrol() {
 
 	// Set booleans for the toggle switches.
